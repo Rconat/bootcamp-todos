bb298138ed7e8c89eed16ae87486297df61e1d22
ryzecous <r3j9c89@gmail.com>
2020-11-21 15:05:03 -0500

    removeTodo

--- script.js
+++ script.js
@@ -14,6 +14,12 @@ function renderTodos() {
         var li = document.createElement('li')
         // add todo text to li
         li.innerText = todos[i]
+        //add data-index attribute
+        li.setAttribute('data-index', i)
+        // create button
+        var button = document.createElement('button')
+        button.innerText = "Complete"
+        li.appendChild(button)
         // append li to todoList
         todoList.appendChild(li)
     }
@@ -34,7 +40,19 @@ function addTodo(event) {
     }
 }
 
-todoForm.addEventListener('submit', addTodo)
+function removeTodo(event) {
+    var target = event.target
+    if (event.target.matches('button')) {
+        // find index from li
+        var index = parseInt(target.parentNode.getAttribute('data-index'))
+        // remove the todo from todos array
+        todos.splice(index, 1)
+        // re-render todos
+        renderTodos()
+    }
+}
 
+todoForm.addEventListener('submit', addTodo)
+TodoList.addEventListener('click', removeTodo)
 
 renderTodos()
