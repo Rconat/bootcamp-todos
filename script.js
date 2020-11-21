684d5eda40afdc523cb501bacc2b4f9e50009f9a
ryzecous <r3j9c89@gmail.com>
2020-11-21 14:37:12 -0500

    addTodo

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ var todoCountSpan = document.querySelector("#todo-count");
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
 
 function renderTodos() {
+    //clear existing todos
+    todoList.innerHTML = ''
+    //re-render todos
     for (var i = 0; i < todos.length; i++) {
         // create li element
         var li = document.createElement('li')
@@ -15,5 +18,23 @@ function renderTodos() {
         todoList.appendChild(li)
     }
 }
+
+function addTodo(event) {
+    event.preventDefault()
+    // get input value
+    var newTodo = todoInput.value
+
+    if (newTodo !== '') {
+        // push text into todos
+        todos.push(newTodo)
+        // clears the input value
+        todoInput.value = null
+        // call renderTodos()
+        renderTodos()
+    }
+}
+
+todoForm.addEventListener('submit', addTodo)
+
 
 renderTodos()
